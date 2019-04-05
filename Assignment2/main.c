30d18284cab04d73e0c0721bfe88f69c794b6d33
Celestine Co <cco@calpoly.edu>
2019-04-05 14:39:26 -0700

    Test turn on red LED

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -10,5 +10,14 @@
  */
 void main(void)
 {
+    uint16_t i, color = 0;
+
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	// TURN ON RED LED
+	P1->SEL0 &= ~BIT0; // P1.0 set as GPIO
+	P1->SEL1 &= ~BIT1;
+
+	P1->DIR |= 1;       // Set Direction to Output
+
 }
