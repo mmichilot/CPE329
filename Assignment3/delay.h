7f159941f924b65e93e9e57e18ac36ac0d212d54
Celestine Co <cco@calpoly.edu>
2019-04-09 18:54:34 -0700

    Added delay files

--- Assignment3/delay.h
+++ Assignment3/delay.h
@@ -1,15 +1,13 @@
 /*
  * delay.h
  *
- *  Created on: Apr 9, 2019
- *      Author: celestine
+ *  Created on: Apr 5, 2019
+ *  Author: Celestine Co & Matthew Michilot
  */
 
 #ifndef DELAY_H_
 #define DELAY_H_
 
-
-
-
+void delay_us(uint32_t delay);
 
 #endif /* DELAY_H_ */
