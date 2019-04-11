6718e0a8abff01ef764016cc1c09435ea8f5dec5
Celestine Co <cco@calpoly.edu>
2019-04-10 22:11:23 -0700

    Updates

--- Assignment3/LCD.c
+++ Assignment3/LCD.c
@@ -40,19 +40,19 @@ void init()
     // 4-bit interface, 2-line display, 5x8 character font
     delay_us(40000);
     P3->OUT &= ~(RS|RW);        // set RS, RW low
-    P3->OUT |= ~EN;             // set EN low
+    P3->OUT &= ~EN;             // set EN low
     P4->OUT = FUNC_SET_1;       // set 1st command 4 MSB
     P3->OUT |= EN;              // set EN high
 
     // wait 37 us
     // function set: RS=0, RW=0, DB7-4=0010, DB3-0=xxxx
     // function set: RS=0, RW=0, DB7-4=NFxx, DB3-0=xxxx
-    command(37, FUNC_SET|LINE_DISP|FONT_DISP);
+    command(37, FUNC_SET|LINE_DISP|(~FONT_DISP));
 
     // wait 37 us
     // function set: RS=0, RW=0, DB7-4=0010, DB3-0=xxxx
     // function set: RS=0, RW=0, DB7-4=NFxx, DB3-0=xxxx
-    command(37, FUNC_SET|LINE_DISP|FONT_DISP);
+    command(37, FUNC_SET|LINE_DISP|(~FONT_DISP));
 
     // wait 37 us
     // function set: RS=0, RW=0, DB7-4=0000, DB3-0=xxxx
@@ -76,30 +76,33 @@ void command(int delay, uint8_t cmd)
     delay_us(delay);
 
     P3->OUT &= ~(RS|RW);        // set RS, RW low
-    P3->OUT |= ~EN;             // set EN low
+    P3->OUT &= ~EN;             // set EN low
     P4->OUT = cmd>>4;           // set 1st command 4 MSB
     P3->OUT |= EN;              // set EN high
+    delay_us(0);
 
     check_busy_flag();
 
-    P3->OUT |= ~EN;             // set EN low
+    P3->OUT &= ~EN;             // set EN low
     P4->OUT = cmd;              // set 2nd command 4 LSB
     P3->OUT |= EN;              // set EN high
     delay_us(0);
 
-    P3->OUT |= ~EN;             // set EN low
+    P3->OUT &= ~EN;             // set EN low
 }
 
 void check_busy_flag()
 {
+    P3->OUT &= ~EN;         // set EN low
+    delay_us(0);
     P4->DIR &= ~BIT3;           // set direction to input
     P3->OUT |= RW;              // set RW high
     delay_us(80);
     while((P4->IN)>>3 == 1)
     {
-        P3->OUT |= ~EN;         // set EN high
+        P3->OUT &= ~EN;         // set EN low
         delay_us(0);
-        P3->OUT |= EN;          // set EN low
+        P3->OUT |= EN;          // set EN high
         delay_us(80);
     }
     P4->DIR &= BIT3;            // set direction back to output
