a5ce06f6286b8696e0dc670088a43b0f713fa4eb
mmichilot <matthew.michilot@gmail.com>
2019-04-12 14:35:31 -0700

    Cleaned up code

--- Assignment3/LCD.c
+++ Assignment3/LCD.c
@@ -7,63 +7,47 @@
  *  Author: Celestine Co & Matthew Michilot
  */
 
+#include <msp.h>
+#include <string.h>
+#include <stdint.h>
+
 #include "delay.h"
-#include "stdint.h"
 #include "LCD.h"
-#include "msp.h"
-#include "string.h"
+
 
 // LCD Helper Functions
-void command(int delay, uint8_t cmd);
-void check_busy_flag();
+void command(uint8_t cmd, int delay);
+void write_char(uint8_t data);
+void output_data(uint8_t data);
 void toggle_EN();
-void write_data(uint8_t data, int delay);
 
-void write_data(uint8_t data, int delay)
-{
-    P3->OUT &= ~EN;         // set EN low
-
-    P4->OUT = (data)>>4;
-    toggle_EN();
-    P4->OUT = (data);
-    toggle_EN();
-    delay_us(delay);
+// Basic LCD Functions
+void clear_LCD() {
+    command(DISP_CLR, 1600);
 }
 
-void clear_LCD()
-{
-    P3->OUT &= ~(RS|RW);    // set RS, RW low
-    write_data(DISP_CLR, 1520);
+void home_LCD() {
+    command(HOME, 1600);
 }
 
-void home_LCD()
-{
-    P3->OUT &= ~(RS|RW);    // set RS, RW low
-    write_data(HOME|BIT1, 1520);
+void set_line(uint8_t line) {
+    command(SET_LINE|line, 40);
 }
 
-void write_char_LCD(uint8_t chr)
-{
-    P3->OUT |= RS;          // set RS high
-    write_data(chr, 40);
-}
-
-void set_line(uint8_t line)
-{
-    P3->OUT &= ~(RS|RW);    // set RS, RW low
-    write_data(SET_LINE|line, 40);
-}
-
-void write_string_LCD(char *str)
-{
+void shift_LCD(uint8_t dir, uint8_t amt) {
     int i;
-    for (i = 0; i < strlen(str); i++){
-        write_char_LCD(str[i]);
-    }
+    for(i = 0; i < amt; i++)
+        command(DISP_SHFT|dir, 40);
 }
 
-void init()
-{
+void write_string(char* str) {
+    int i;
+    for (i = 0; i < strlen(str); i++)
+        write_char(str[i]);
+
+}
+
+void init() {
     // wait 40 ms
     // function set: RS=0, RW=0, DB=0011xxxx
     // 4-bit interface, 2-line display, 5x8 character font
@@ -78,60 +62,56 @@ void init()
 
     // wait 37 us
     // function set: RS=0, RW=0, DB=0010NFxx
-    command(40, FUNC_SET|LINE_DISP);
+    command(FUNC_SET|LINE_DISP, 40);
 
     // wait 37 us
     // function set: RS=0, RW=0, DB=0010NFxx
-    command(40, FUNC_SET|LINE_DISP);
+    command(FUNC_SET|LINE_DISP, 40);
 
     // wait 37 us
     // function set: RS=0, RW=0, DB=00001DCB
-    command(40, DISP_CTRL|DISP_ON|CURS_ON|BLINK_ON);
+    command(DISP_CTRL|DISP_ON|CURS_ON|BLINK_ON, 40);
 
     // wait 37 us
     // disp on/off: RS=0, RW=0, DB=00000001
-    command(1600, DISP_CLR);
+    command(DISP_CLR, 1600);
 
     // wait 1.52 ms
     // entry mode: RS=0, RW=0, DB=00000111
-    command(40, ENTRY_MODE|DIRECTION);
+    command(ENTRY_MODE|DIRECTION, 40);
 }
 
-void command(int delay, uint8_t cmd)
-{
+void command(uint8_t cmd, int delay) {
     P3->OUT &= ~(RS|RW);        // set RS, RW low
+
     P3->OUT &= ~EN;             // set EN low
 
-    P4->OUT = cmd>>4;           // set 1st command 4 MSB
-    toggle_EN();
-
-    P4->OUT = cmd;              // set 2nd command 4 LSB
-    toggle_EN();
+    output_data(cmd);
 
     delay_us(delay);
 }
 
-void toggle_EN()
-{
-    P3->OUT |= EN;              // set EN high
-    delay_us(0);
-    P3->OUT &= ~EN;             // set EN low
+void write_char(uint8_t chr) {
+    P3->OUT &= ~RW;         // set RW low
+    P3->OUT |= RS;          // set RS high
+
+    P3->OUT &= ~EN;         // set EN low
+
+    output_data(chr);
+
+    delay_us(40);
 }
 
-void check_busy_flag()
-{
-    P3->OUT &= ~EN;         // set EN low
+void toggle_EN() {
+    P3->OUT |= EN;          // set EN high
     delay_us(0);
-    P4->DIR &= ~BIT3;           // set direction to input
-    P3->OUT |= RW;              // set RW high
-    delay_us(80);
-    while((P4->IN && 0x04)>>3 == 1)
-    {
-        P3->OUT |= EN;          // set EN high
-       delay_us(0);
-       P3->OUT &= ~EN;         // set EN low
-       delay_us(80);
-    }
-    P4->DIR &= BIT3;            // set direction back to output
-    P3->OUT |= ~RW;             // set RW low
+    P3->OUT &= ~EN;         // set EN low
+}
+
+void output_data(uint8_t data) {
+    P4->OUT = data>>4;
+    toggle_EN();
+
+    P4->OUT = data;
+    toggle_EN();
 }
--- Assignment3/main.c
+++ Assignment3/main.c
@@ -5,7 +5,9 @@
  *    Author: Celestine Co & Matthew Michilot
  */
 
-#include "msp.h"
+#include <msp.h>
+
+#include "delay.h"
 #include "LCD.h"
 #include "set_dco.h"
 
@@ -13,18 +15,20 @@ void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
 
+	// LCD Data Bus Setup
     P4->SEL0 &= ~(BIT3|BIT2|BIT1|BIT0);         // P4.0-3 = Data Bus
     P4->SEL1 &= ~(BIT3|BIT2|BIT1|BIT0);
     P4->DIR |= BIT3|BIT2|BIT1|BIT0;             // set direction to output
 
+    // LCD Control Signal Setup
     P3->SEL0 &= ~(BIT3|BIT2|BIT0);              // P3.0=RW, 3.2=RS, 3.3=EN
     P3->SEL1 &= ~(BIT3|BIT2|BIT0);
     P3->DIR |= BIT3|BIT2|BIT0;                  // set direction to output
 
+    // LCD Program
     init();
     set_line(LINE1);
-    write_string_LCD("Hello World");
+    write_string("Hello World");
     set_line(LINE2);
-    write_string_LCD("Assignment 3");
-    home_LCD();
+    write_string("Assignment 3");
 }
