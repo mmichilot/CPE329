26d4de3ffc21d5b8897b6751bfbcff3917a3e4d5
Celestine Co <cco@calpoly.edu>
2019-04-17 14:22:37 -0700

    Check pin

--- Project1/digi_lock.c
+++ Project1/digi_lock.c
@@ -38,8 +38,9 @@ void wait() {
 }
 
 void locked_state() {
-    uint8_t input, pin1, pin2, pin3, pin4;
+    uint8_t input;
 
+    clear_LCD();
     set_line(LINE1);
     write_string("LOCKED");
     set_line(LINE2);
@@ -54,21 +55,11 @@ void locked_state() {
            delay_us(40000);
        }
 
+       if (input == 10)
+           locked_state();
+
        if (get_addr() == EOP) {
-           pin1 = read_data(PIN1);
-           pin2 = read_data(PIN2);
-           pin3 = read_data(PIN3);
-           pin4 = read_data(PIN4);
-
            check_pin();
-           loading_screen();
-           write_string("UNLOCKED");
-           set_line(LINE2);
-
-           write_char(pin1);
-           write_char(pin2);
-           write_char(pin3);
-           write_char(pin4);
        }
     }
 
@@ -76,10 +67,26 @@ void locked_state() {
 }
 
 void unlocked_state() {
+    loading_screen();
 
+    clear_LCD();
+    write_string("UNLOCKED");
 }
 
 void check_pin() {
+    uint8_t pin1, pin2, pin3, pin4;
+    char mypin[] = "1234";
+
+    pin1 = read_data(PIN1);
+    pin2 = read_data(PIN2);
+    pin3 = read_data(PIN3);
+    pin4 = read_data(PIN4);
+
+    if (pin1 == mypin[0] && pin2 == mypin[1] && pin3 == mypin[2] && pin4 == mypin[3]) {
+        unlocked_state();
+    } else {
+        locked_state();
+    }
 
 }
 
