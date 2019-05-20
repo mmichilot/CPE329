e5e58cda32477aa86d085056d9c429b74356c68f
mmichilot <matthew.michilot@gmail.com>
2019-05-19 23:50:50 -0700

    Changed frequency stuff

--- Project3/dmm.c
+++ Project3/dmm.c
@@ -11,33 +11,44 @@
 volatile uint32_t count = 0;
 volatile uint32_t num_trigs = 0;
 volatile uint32_t curr_freq = 0;
+volatile uint32_t flag = 0;
 
 int get_freq(void) {
-//    curr_freq = 0;
-//    count = 0;
-//    num_trigs = 0;
-//
-//    TIMER_A0->CCR[0] += 48000;  // add 0.75s delay
-//    TIMER_A0->CTL |= TIMER_A_CTL_CLR;   // clear current count
-//    TIMER_A0->CCTL[1] |= TIMER_A_CCTLN_CM_1;    // enable capture
-//    TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_CCIE;    // enable CCR0 interrupt
+    curr_freq = 0;
+    count = 0;
+    num_trigs = 0;
+
+    TIMER_A0->CCTL[1] |= TIMER_A_CCTLN_CM_1;    // enable capture
+    TIMER_A0->CCR[0] += 48000;  // add 0.75s delay
+    TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_CCIE;    // enable CCR0 interrupt
+    TIMER_A0->CTL |= TIMER_A_CTL_CLR;   // clear current count
+
+    while(flag == 0);
 
     return curr_freq;
 }
 
-//void TA0_0_IRQHandler(void) {
-//    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;  // clear interrupt flag
-//    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIE;   // disable CCR0 interrupt
-//
-//    TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CM_1;   // disable capture if it isn't disabled
-//
-//}
+void TA0_0_IRQHandler(void) {
+    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;  // clear interrupt flag
+    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIE;   // disable CCR0 interrupt
+    TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CM_1;   // disable capture if it isn't disabled
+
+    // calculate frequency if AC
+    if (count != 0) {
+        curr_freq = (int) (64000/count);    // calculate freq
+        if (count > 64000)      // edge case where freq. is 1
+            curr_freq++;
+    }
+
+    flag = 1;
+}
 
 void TA0_N_IRQHandler(void) {
     uint32_t tmp_count;
 
     if (TIMER_A0->CCTL[1] & TIMER_A_CCTLN_CCIFG) {
 
+        TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIE;   // temporarily disable CCR0 interrupts
         TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG;  // clear interrupt flag
         tmp_count = TIMER_A0->CCR[1];   // capture count
 
@@ -46,19 +57,14 @@ void TA0_N_IRQHandler(void) {
         else
             count = tmp_count - count;
 
-        if (num_trigs == 2) {
-            // calculate frequency
-            curr_freq = (int) (64000/count);    // calculate freq
-
-            if (count > 64000)      // edge case where freq. is 1
-                curr_freq++;
-
-            count = 0;
-            num_trigs = 0;
-        }
+        if (num_trigs == 2)
+            TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CM_1;   // disable capture
         else {
             num_trigs++;
+            TIMER_A0->CCR[0] += 48000;   // push back timer;
         }
+
+        TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_CCIE;   // re-enable CCR0 interrupts
     }
 }
 
@@ -81,6 +87,6 @@ void init_timer(void) {
                          TIMER_A_CCTLN_CCIE |   // use capture mode
                          TIMER_A_CCTLN_SCS;     // synchronous capture
 
-//    NVIC->ISER[0] = 1 << (TA0_0_IRQn & 31); // enable CCR0 ISR
+    NVIC->ISER[0] = 1 << (TA0_0_IRQn & 31); // enable CCR0 ISR
     NVIC->ISER[0] = 1 << (TA0_N_IRQn & 31); // enable CCR1 ISR
 }
